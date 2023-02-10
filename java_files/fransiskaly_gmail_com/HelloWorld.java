835e13b909aad0734b70ee2fe1e9032e80395aeb
fransiskalidya <fransiskaly@gmail.com>
2023-02-10 12:43:12 +0700

    new test

---
+++ java_files/fransiskaly_gmail_com/HelloWorld.java
@@ -0,0 +1,7 @@
+package fransiskaly_gmail_com; 
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.print("Hello World!"); 
+    }
+}
---
+++ java_files/riko_gmail_com/HelloWorld.java
@@ -0,0 +1,8 @@
+package riko_gmail_com; 
+
+public class HelloWorld {
+
+    public static void main(String[] args) {
+     System.out.print("Hello World!"); 
+   }
+}
